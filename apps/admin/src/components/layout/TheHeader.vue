300271e38daac0167d4e9339776d0626d8c4b8ff
sskim950105 <sskim950105@gmail.com>
2024-03-10 22:59:53 +0900

    chore: add tailwind css & daisy ui

---
+++ apps/admin/src/components/layout/TheHeader.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<style scoped></style>
--- apps/admin/src/views/HomeView.vue
+++ apps/admin/src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Home</h1>
+    <button class="btn btn-info">Info</button>
+    <button class="btn btn-success">Success</button>
+    <button class="btn btn-warning">Warning</button>
+    <button class="btn btn-error">Error</button>
+  </div>
 </template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
